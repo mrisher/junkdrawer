e0ba1ca25037ad4b62058ccde650daff16450445
Mark Risher <218038+mrisher@users.noreply.github.com>
2023-01-13 14:26:38 +0100

    removed the css package

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,8 +5,8 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {});
 
-  app.import('node_modules/todomvc-common/base.css');
-  app.import('node_modules/todomvc-app-css/index.css');
+  //app.import('node_modules/todomvc-common/base.css');
+  //app.import('node_modules/todomvc-app-css/index.css');
 
   // Use `app.import` to add additional libraries to the generated
   // output files.
