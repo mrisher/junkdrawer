7239bcb26399feca635becb12cb6182943ed90de
Mark Risher <218038+mrisher@users.noreply.github.com>
2023-02-12 18:39:57 +0100

    application test adds an entity

--- tests/acceptance/junkdrawer-test.js
+++ tests/acceptance/junkdrawer-test.js
@@ -1,5 +1,12 @@
 import { module, test } from 'qunit';
-import { visit, click, currentURL } from '@ember/test-helpers';
+import { 
+  visit, 
+  click, 
+  currentURL, 
+  triggerKeyEvent, 
+  settled, 
+  fillIn,
+} from '@ember/test-helpers';
 import { setupApplicationTest } from 'junkdrawer/tests/helpers';
 
 module('Acceptance | junkdrawer', function (hooks) {
@@ -7,10 +14,17 @@ module('Acceptance | junkdrawer', function (hooks) {
 
   test('visiting /', async function (assert) {
     await visit('/');
-
     assert.strictEqual(currentURL(), '/');
     assert.dom('h1').hasText('The Junk Drawer');
-
     assert.dom('a.ember-view').exists({ count: 3 });
+  });
+
+  test('entity count', async function (assert) { 
+    await visit('/');
+    assert.dom('input.new-todo').exists();
+    await fillIn('input.new-todo', 'test_todo_123');
+    await triggerKeyEvent('input.new-todo', 'keydown', 'Enter');
+    await settled();
+    assert.dom('.todo-count strong').hasText('1');
   });
 });
